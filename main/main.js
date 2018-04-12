f6732c2536a082736004a1e430f28fe67ec16360
l-ChengYou-l <l-chenchen-l@hotmail.com>
2018-04-12 13:19:55 +0800

    猜数

--- main/main.js
+++ main/main.js
@@ -1,6 +1,18 @@
 // Write your cade below:
-function main() {};
-
-module.exports = {
-    main
-}
+function guess_number(system,user) {
+    let count = [0,0]
+    for(let i = 0;i < system.length;i++){
+        for(let j = 0;j < user.length;j++){
+            if(system[i] == user[j]){
+                if(i==j){
+                    count[0]++
+                }else{
+                    count[1]++
+                }
+            }
+        }
+    }
+    console.log(`${count[0]}A${count[1]}B`)
+    return `${count[0]}A${count[1]}B`
+};
+module.exports = guess_number;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,26 @@
-const main = require('../main/main');
+const guess_number = require('../main/main');
+let system = '1234'
+describe('guess_number', () => {
+    it('all correct',function(){
+        let user = '1234'
+        let result = guess_number(system,user)
+        expect(result).toEqual('4A0B')
+    });
+    it('numer correst but location wrong',function(){
+        let user = '4321'
+        let result = guess_number(system,user)
+        expect(result).toEqual('0A4B')
+    });
+    it('some correct but other wrong',function(){
+        let user = '1526'
+        let result = guess_number(system,user)
+        expect(result).toEqual('1A1B')
+    });
+    it('all wrong',function(){
+        let user = '0000'
+        let result = guess_number(system,user)
+        expect(result).toEqual('0A0B')
+    });
 
-describe('main()', () => {
 
 });
